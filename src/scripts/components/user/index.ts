313d7c6f0b7ab41af263f7de614ee3d746f92cfe
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-02-04 22:40:28 +0200

    feat: registration and signIn basic without beauty

--- src/scripts/components/user/index.ts
+++ src/scripts/components/user/index.ts
@@ -1,10 +1,12 @@
 import BaseComponent from '../base';
 import { createSpan, createDiv, createInput, createButton } from '../../utils';
 import { apiService } from '../../api/apiMethods';
-import { getState } from '../../state';
+import { updateState, getState } from '../../state';
 // import '../../api/testingApiMethods';
 
 export default class User extends BaseComponent {
+  logoUser: HTMLElement | undefined;
+
   modalWindow: HTMLDivElement | undefined;
 
   modalOverlay: HTMLDivElement | undefined;
@@ -19,7 +21,9 @@ export default class User extends BaseComponent {
 
   checkBox: HTMLInputElement | undefined;
 
-  submitBtn: HTMLButtonElement | undefined;
+  registrationBtn: HTMLButtonElement | undefined;
+
+  loginBtn: HTMLButtonElement | undefined;
 
   changeModalBtnL: HTMLButtonElement | undefined;
 
@@ -32,13 +36,15 @@ export default class User extends BaseComponent {
   }
 
   public createHTML(): void {
-    const logoUser = createSpan({
+    this.logoUser = createSpan({
       text: 'Юзер',
       className: 'logoUser',
     });
+    if (getState().userEmail) {
+      this.disableLogo();
+    }
     this.modalWindow = createDiv({ className: 'modal-user' });
     this.modalOverlay = createDiv({ className: 'modal-overlay' });
-
 
     const modalTitleR = createSpan({
       text: 'Registration',
@@ -70,12 +76,16 @@ export default class User extends BaseComponent {
       className: 'modal-user__checkbox',
       type: 'checkbox',
     });
-    this.submitBtn = createButton({
-      className: 'modal-user__submit',
-      text: 'SIGN IN',
-      action: 'submit-user',
+    this.registrationBtn = createButton({
+      className: 'modal-user__submit-registration registration',
+      text: 'REGISTRATION',
+      action: 'registrate-user',
     });
-
+    this.loginBtn = createButton({
+      className: 'modal-user__submit-login login',
+      text: 'SIGN IN',
+      action: 'login-user',
+    });
     this.changeModalBtnR = createButton({
       className: 'modal-user__change-modal registration',
       text: 'Do you have an account? Sign In',
@@ -95,14 +105,15 @@ export default class User extends BaseComponent {
     this.modalWindow.append(this.inputPassword);
     this.modalWindow.append(this.checkLabel);
     this.checkLabel.append(this.checkBox);
-    this.modalWindow.append(this.submitBtn);
+    this.modalWindow.append(this.registrationBtn);
+    this.modalWindow.append(this.loginBtn);
     this.modalWindow.append(this.changeModalBtnL);
     this.modalWindow.append(this.changeModalBtnR);
 
 
     document.body.append(this.modalOverlay);
     this.fragment.append(this.modalWindow);
-    this.fragment.append(logoUser);
+    this.fragment.append(this.logoUser);
   }
 
   public listenEvents(): void {
@@ -111,27 +122,92 @@ export default class User extends BaseComponent {
     (this.changeModalBtnR as HTMLButtonElement).addEventListener('click', this.toggleRegisterModal.bind(this));
     (this.changeModalBtnL as HTMLButtonElement).addEventListener('click', this.toggleRegisterModal.bind(this));
     (this.checkBox as HTMLInputElement).addEventListener('change', this.togglePasswordView.bind(this));
-    (this.submitBtn as HTMLInputElement).addEventListener('click', this.createOrSignInUser.bind(this));
+    (this.registrationBtn as HTMLInputElement).addEventListener('click', this.registrateAndLoginUser.bind(this));
+    (this.loginBtn as HTMLInputElement).addEventListener('click', this.loginUser.bind(this, undefined));
   }
 
-  async createOrSignInUser() {
-    console.log('creating user....');
+  async registrateAndLoginUser() {
+    let isValid = true;
+    const name = (this.inputName as HTMLInputElement).value.trim();
 
-    const name = 'd';
-    const email = 'dd@mail.com';
-    const password = '11111111';
-    const user = { name, email, password };
-    await apiService.createUser(user);
+    if (!name.length) {
+      console.log('Name is required field');
+      isValid = false;
+    }
 
-    const userFromBase = await apiService.getUser(getState().userId);
-    console.log('back answers....');
-    console.log(userFromBase);
+    if (isValid && this.verifyUser()) {
+      const email = this.verifyUser()?.email as string;
+      const password = this.verifyUser()?.password as string;
 
-    // failed to connect
+      try {
+        await apiService.createUser({ name, email, password });
+      } catch (error) {
+        console.log('such User exists!!!!');
+        return;
+      }
+      this.loginUser(name);
+    }
   }
 
-  SignInUser() {
+  verifyUser() {
+    const email = (this.inputMail as HTMLInputElement).value;
+    const password = (this.inputPassword as HTMLInputElement).value.trim();
+    const reg = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+    const minPasswordLength = 8;
+    let isValid = true;
 
+    if (reg.test(email) === false) {
+      console.log('Email should have correct format');
+      isValid = false;
+    }
+
+    if (password.length < minPasswordLength) {
+      console.log('Password is too short - should be 8 chars minimum.');
+      isValid = false;
+    }
+
+    if (isValid) {
+      return { email, password };
+    }
+  }
+
+  async loginUser(name?: string) {
+    if (this.verifyUser()) {
+      const email = this.verifyUser()?.email as string;
+      const password = this.verifyUser()?.password as string;
+
+      try {
+        await apiService.loginUser({ email, password });
+
+        if (name) {
+          updateState({
+            userName: name,
+            userEmail: email,
+            userPassword: password,
+          });
+        } else {
+          updateState({
+            userEmail: email,
+            userPassword: password,
+          });
+        }
+
+        this.closeModalWindow();
+        // todo change logoUser
+        this.disableLogo();
+      } catch (error) {
+        console.log('Incorrect e-mail or password!!!!');
+        return;
+      }
+    }
+  }
+
+  disableLogo() {
+    console.log('changing logo');
+    if (this.logoUser) {
+      this.logoUser.textContent = getState().userName + ' ' + getState().userEmail;
+      this.logoUser.style.pointerEvents = 'none';
+    }
   }
 
   togglePasswordView() {
