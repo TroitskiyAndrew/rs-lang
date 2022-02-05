30478b916ff0abf3c64d81203f0120fa96195fbb
Essonti <Strofim4enko@yandex.ru>
2022-02-06 01:39:45 +0300

    feat: implement start view of 'Sprint' game

--- src/scripts/components/games/sprintGame/index.ts
+++ src/scripts/components/games/sprintGame/index.ts
@@ -1,5 +1,5 @@
 import BaseComponent from '../../base';
-import { createDiv, createSpan } from '../../../utils';
+import { createButton, createDiv, createSpan } from '../../../utils';
 import { pageChenging } from '../../../rooting';
 
 export default class SprintGame extends BaseComponent {
@@ -11,5 +11,33 @@ export default class SprintGame extends BaseComponent {
   public oninit(): Promise<void> {
     pageChenging(createSpan({ text: 'Спринт Игра' }), this.name);
     return Promise.resolve();
+  }
+
+  public createHTML(): void {
+    const page = createDiv({ className: 'page sprint' });
+    const groupsWrapper = createDiv({ className: 'sprint__groups-wrapper'});
+    const groupsWrapperText = createDiv({ className: 'sprint__groups-wrapper_text' });
+    const groupsWrapperButtons = createDiv({ className: 'sprint__groups-wrapper_buttons' });
+    
+    groupsWrapperText.innerHTML =  `
+    <h3>Спринт</h3>
+    <p class = "sprint__groups-wrapper_paragraph">Тренирует навык быстрого перевода с английского языка на русский. 
+    Вам нужно выбрать соответствует ли перевод предложенному слову.</p>
+    `;
+
+    for (let i=0; i < 6; i++) {
+      groupsWrapperButtons.append(createButton({ text: `${i + 1}`, className: `group-${i}`, action: 'showSprintGame'}));
+    }
+    
+    groupsWrapper.append(groupsWrapperText);
+    groupsWrapper.append(groupsWrapperButtons);
+
+    page.append(groupsWrapper);
+    this.fragment.append(page);
+  }
+
+  private chooseGroup(): void {
+    createDiv({className: "groups-wrapper"});
+
   }
 }
