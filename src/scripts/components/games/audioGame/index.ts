96d61c21c99ca983eba438708f143be2963a8541
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-02-05 20:37:48 +0200

    init: audio game

--- src/scripts/components/games/audioGame/index.ts
+++ src/scripts/components/games/audioGame/index.ts
@@ -1,8 +1,12 @@
 import BaseComponent from '../../base';
-import { createDiv, createSpan } from '../../../utils';
 import { pageChenging } from '../../../rooting';
+import { createSpan, createDiv, createInput, createButton } from '../../../utils';
+import { apiService } from '../../../api/apiMethods';
+import { updateState, getState } from '../../../state';
 
 export default class AudioGame extends BaseComponent {
+  test: HTMLElement | undefined;
+
   constructor(elem: HTMLElement) {
     super(elem);
     this.name = 'audioGame';
@@ -11,5 +15,14 @@ export default class AudioGame extends BaseComponent {
   public oninit(): Promise<void> {
     pageChenging(createSpan({ text: 'Аудио Игра' }), this.name);
     return Promise.resolve();
+  }
+
+  public createHTML(): void {
+
+    this.test = createSpan({
+      text: 'test span Audio game',
+    });
+
+    this.fragment.append(this.test);
   }
 }
