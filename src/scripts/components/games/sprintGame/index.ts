fe5b2f146e859a53f81f14c87f46300c5cec900b
Essonti <Strofim4enko@yandex.ru>
2022-02-06 13:26:30 +0300

    feat: start implementing transfer from start view to 'Sprint' game

--- src/scripts/components/games/sprintGame/index.ts
+++ src/scripts/components/games/sprintGame/index.ts
@@ -3,19 +3,24 @@ import { createButton, createDiv, createSpan } from '../../../utils';
 import { pageChenging } from '../../../rooting';
 
 export default class SprintGame extends BaseComponent {
+
+  groupsWrapper: HTMLElement | undefined;
+  groupsWrapperButton: HTMLButtonElement | undefined;
+
   constructor(elem: HTMLElement) {
     super(elem);
     this.name = 'sprintGame';
   }
 
   public oninit(): Promise<void> {
+    this.setActions();
     pageChenging(createSpan({ text: 'Спринт Игра' }), this.name);
     return Promise.resolve();
   }
 
   public createHTML(): void {
     const page = createDiv({ className: 'page sprint' });
-    const groupsWrapper = createDiv({ className: 'sprint__groups-wrapper'});
+    this.groupsWrapper = createDiv({ className: 'sprint__groups-wrapper'});
     const groupsWrapperText = createDiv({ className: 'sprint__groups-wrapper_text' });
     const groupsWrapperButtons = createDiv({ className: 'sprint__groups-wrapper_buttons' });
     
@@ -26,18 +31,27 @@ export default class SprintGame extends BaseComponent {
     `;
 
     for (let i=0; i < 6; i++) {
-      groupsWrapperButtons.append(createButton({ text: `${i + 1}`, className: `group-${i}`, action: 'showSprintGame'}));
+      this.groupsWrapperButton = createButton({ text: `${i + 1}`, className: `group-${i}`, action: 'showSprintGame'});
+      groupsWrapperButtons.append(this.groupsWrapperButton);
     }
     
-    groupsWrapper.append(groupsWrapperText);
-    groupsWrapper.append(groupsWrapperButtons);
+    this.groupsWrapper.append(groupsWrapperText);
+    this.groupsWrapper.append(groupsWrapperButtons);
 
-    page.append(groupsWrapper);
+    page.append(this.groupsWrapper);
     this.fragment.append(page);
   }
 
-  private chooseGroup(): void {
-    createDiv({className: "groups-wrapper"});
+  public listenEvents(): void {
+    this.groupsWrapper!.addEventListener('click', this.actionHandler.bind(this));
+  }
 
+  public setActions(): void {
+    this.actions.showSprintGame = this.showSprintGame;
+  }
+
+  public showSprintGame(): void {
+    console.log(this.groupsWrapperButton?.className.slice(-1))
+    //this.groupsWrapper!.style.display = 'none';
   }
 }
