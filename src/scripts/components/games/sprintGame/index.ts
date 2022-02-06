fef7798dd4111be5991bb1a71958cbd1b88f96fb
Essonti <Strofim4enko@yandex.ru>
2022-02-06 23:26:48 +0300

    feat: start implementing 'sprint' game without transfer

--- src/scripts/components/games/sprintGame/index.ts
+++ src/scripts/components/games/sprintGame/index.ts
@@ -4,8 +4,8 @@ import { pageChenging } from '../../../rooting';
 
 export default class SprintGame extends BaseComponent {
 
-  groupsWrapper: HTMLElement | undefined;
-  groupsWrapperButton: HTMLButtonElement | undefined;
+  // groupsWrapper: HTMLElement | undefined;
+  // groupsWrapperButton: HTMLButtonElement | undefined;
 
   constructor(elem: HTMLElement) {
     super(elem);
@@ -20,38 +20,63 @@ export default class SprintGame extends BaseComponent {
 
   public createHTML(): void {
     const page = createDiv({ className: 'page sprint' });
-    this.groupsWrapper = createDiv({ className: 'sprint__groups-wrapper'});
-    const groupsWrapperText = createDiv({ className: 'sprint__groups-wrapper_text' });
-    const groupsWrapperButtons = createDiv({ className: 'sprint__groups-wrapper_buttons' });
-    
-    groupsWrapperText.innerHTML =  `
-    <h3>Спринт</h3>
-    <p class = "sprint__groups-wrapper_paragraph">Тренирует навык быстрого перевода с английского языка на русский. 
-    Вам нужно выбрать соответствует ли перевод предложенному слову.</p>
-    `;
+    const sprintWrapper = createDiv({ className: 'sprint-wrapper' });
 
-    for (let i=0; i < 6; i++) {
-      this.groupsWrapperButton = createButton({ text: `${i + 1}`, className: `group-${i}`, action: 'showSprintGame'});
-      groupsWrapperButtons.append(this.groupsWrapperButton);
-    }
-    
-    this.groupsWrapper.append(groupsWrapperText);
-    this.groupsWrapper.append(groupsWrapperButtons);
+    const paramsWrapper = createDiv({ className: 'params-wrapper' });
+    const paramsLevelWrapper = createDiv({ className: 'params-wrapper__level' });
+    const paramsMultiplyerWrapper = createDiv({ className: 'params-wrapper__multiplyer' });
+    const paramsTimeWrapper = createDiv({ className: 'params-wrapper__time' });
 
-    page.append(this.groupsWrapper);
+    const marioWrapper = createDiv({ className: 'mario-wrapper' });
+    const marioGifWrapper = createDiv({ className: 'mario-wrapper__gif' });
+    
+    const wordsWrapper = createDiv({ className: 'words-wrapper' });
+    const buttonsWrapper = createDiv({ className: 'buttons-wrapper' });
+
+    const star = document.createElement('img');
+    star.src = '/../../../../assets/img/sprintGame/png/pixel_star_50px.png';
+    console.log(star)
+
+    paramsLevelWrapper.append(star);
+    paramsWrapper.append(paramsLevelWrapper);
+    sprintWrapper.append(paramsWrapper);
+
+    page.append(sprintWrapper);
     this.fragment.append(page);
+    
+
+
+    // const groupsWrapperText = createDiv({ className: 'sprint__groups-wrapper_text' });
+    // const groupsWrapperButtons = createDiv({ className: 'sprint__groups-wrapper_buttons' });
+    
+    // groupsWrapperText.innerHTML =  `
+    // <h3>Спринт</h3>
+    // <p class = "sprint__groups-wrapper_paragraph">Тренирует навык быстрого перевода с английского языка на русский. 
+    // Вам нужно выбрать соответствует ли перевод предложенному слову.</p>
+    // `;
+
+    // for (let i=0; i < 6; i++) {
+    //   this.groupsWrapperButton = createButton({ text: `${i + 1}`, className: `group-${i}`, action: 'showSprintGame'});
+    //   groupsWrapperButtons.append(this.groupsWrapperButton);
+    // }
+    
+    // this.groupsWrapper.append(groupsWrapperText);
+    // this.groupsWrapper.append(groupsWrapperButtons);
+
+    // page.append(this.groupsWrapper);
+    // this.fragment.append(page);
   }
 
-  public listenEvents(): void {
-    this.groupsWrapper!.addEventListener('click', this.actionHandler.bind(this));
-  }
+  // public listenEvents(): void {
+  //   this.groupsWrapper!.addEventListener('click', this.actionHandler.bind(this));
+  // }
 
-  public setActions(): void {
-    this.actions.showSprintGame = this.showSprintGame;
-  }
+  // public setActions(): void {
+  //   this.actions.showSprintGame = this.showSprintGame;
+  // }
 
-  public showSprintGame(): void {
-    console.log(this.groupsWrapperButton?.className.slice(-1))
-    //this.groupsWrapper!.style.display = 'none';
-  }
+  // public showSprintGame(): void {
+  //   console.log(this.groupsWrapperButton?.className.slice(-1))
+  //   //this.groupsWrapper!.style.display = 'none';
+  // }
 }
