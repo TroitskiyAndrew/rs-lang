5ffe1d5a882d9b3f61d2439c8d898a216037f1d5
Essonti <Strofim4enko@yandex.ru>
2022-02-09 17:18:19 +0300

    feat: add coin counter to structure

--- src/scripts/components/games/sprintGame/index.ts
+++ src/scripts/components/games/sprintGame/index.ts
@@ -35,7 +35,6 @@ export default class SprintGame extends BaseComponent {
     const gamepadWrapper = createDiv({ className: 'gamepad-wrapper'});
     const wordsWrapper = createDiv({ className: 'words-wrapper' });
 
-    
     this.renderParams(paramsWrapper);
     this.renderMario(marioWrapper);
     this.renderGamepad(gamepadWrapper);
@@ -50,26 +49,29 @@ export default class SprintGame extends BaseComponent {
     this.fragment.append(page);
   }
 
+  private getCoin(size: string) {
+    const coin = document.createElement('img');
+    coin.className = `coin-img_${size}`
+    coin.src = '/../../../../assets/img/sprintGame/gif/CoinSMW.gif';
+    return coin;
+  }
+
   private renderParams(paramsWrapper: HTMLDivElement) {
     const paramsLevelWrapper = createDiv({ className: 'params-wrapper__level' });
     const paramsMultiplyerWrapper = createDiv({ className: 'params-wrapper__multiplyer' });
     const paramsTime = createDiv({ className: 'params-wrapper__time' });
-    const paramsScore = createDiv({ className: 'params-wrapper__score'});
+    const paramsCoinsWrapper = createDiv({ className: 'params-wrapper__coins-wrapper' });
+    const paramsCoins = createDiv({ className: 'params-wrapper__coins' });
+    const paramsScore = createDiv({ className: 'params-wrapper__score' });
 
     const star = document.createElement('img');
     star.className = 'group-img'
     star.src = '/../../../../assets/img/sprintGame/png/pixel_star_50px.png';
-
-    // const multiplyerBorderImg = document.createElement('img');
-    // multiplyerBorderImg.className = 'params-wrapper__multiplyer-border';
-    // multiplyerBorderImg.src = '/../../../../assets/img/sprintGame/png/ItemStock-cut.png';
     
-    // TEST
     const multiplyerItem = document.createElement('img');
     multiplyerItem.className = 'params-wrapper__multiplyer-item';
     multiplyerItem.src = '/../../../../assets/img/sprintGame/png/MushroomSMW.png';
-    // TEST
-
+    
     if (this.options) {
       options = JSON.parse (this.options);
       localStorage.setItem ('options', this.options);
@@ -93,12 +95,17 @@ export default class SprintGame extends BaseComponent {
     paramsMultiplyerWrapper.append(multiplyerItem);
 
     paramsTime.innerHTML = `time<br> 60`;
+    
+    paramsCoins.append(this.getCoin('small'))
+    paramsCoins.innerHTML += `&#215; ${'1'}`
     paramsScore.textContent = '0';
-
+    paramsCoinsWrapper.append(paramsCoins);
+    paramsCoinsWrapper.append(paramsScore);
+    
     paramsWrapper.append(paramsLevelWrapper);
     paramsWrapper.append(paramsMultiplyerWrapper);
     paramsWrapper.append(paramsTime);
-    paramsWrapper.append(paramsScore);
+    paramsWrapper.append(paramsCoinsWrapper);
   }
 
   private renderMario(marioWrapper: HTMLDivElement) {
@@ -106,12 +113,8 @@ export default class SprintGame extends BaseComponent {
     mario.className = 'mario-img'
     mario.src = '/../../../../assets/img/sprintGame/gif/Mario_SMW.gif';
 
-    const coin = document.createElement('img');
-    coin.className = 'coin-img'
-    coin.src = '/../../../../assets/img/sprintGame/gif/CoinSMW.gif';
-
     marioWrapper.append(mario);
-    marioWrapper.append(coin);
+    marioWrapper.append(this.getCoin('medium'));
   }
 
   private renderGamepad(gamepadWrapper: HTMLDivElement) {
@@ -170,6 +173,11 @@ export default class SprintGame extends BaseComponent {
 
 }
 
+
+
+  // const multiplyerBorderImg = document.createElement('img');
+  // multiplyerBorderImg.className = 'params-wrapper__multiplyer-border';
+  // multiplyerBorderImg.src = '/../../../../assets/img/sprintGame/png/ItemStock-cut.png';
 
   // public listenEvents(): void {
   //   this.groupsWrapper!.addEventListener('click', this.actionHandler.bind(this));
