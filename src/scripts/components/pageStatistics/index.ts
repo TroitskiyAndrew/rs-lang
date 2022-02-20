7b2556640ed3e48b0080da7a7b0ab1b3743d75cc
Andrew <TroitskiyA@yandex.ru>
2022-02-20 15:51:46 +0500

    feat: add c3

--- src/scripts/components/pageStatistics/index.ts
+++ src/scripts/components/pageStatistics/index.ts
@@ -1,8 +1,11 @@
 import BaseComponent from '../base';
 import { createDiv, createSpan } from '../../utils';
 import { pageChenging } from '../../rooting';
+import c3 from 'c3';
 
 export default class PageStatistics extends BaseComponent {
+  // statistic: Statistics;
+
   constructor(elem: HTMLElement) {
     super(elem);
     this.name = 'pageStatistics';
@@ -10,12 +13,21 @@ export default class PageStatistics extends BaseComponent {
 
   public oninit(): Promise<void> {
     pageChenging(createSpan({ text: 'Статистика' }), this.name);
+    c3.generate({
+      bindto: '.testChart',
+      data: {
+        columns: [
+          ['data1', 1, 2, 1, 2, 0],
+          ['data2', 0, 2, 1, 2, 0],
+        ],
+      },
+    });
     return Promise.resolve();
   }
 
   public createHTML(): void {
     const page = createDiv({ className: 'page statistics' });
-    page.append(createSpan({ text: 'Статистика' }));
+    page.append(createDiv({ className: 'testChart' }));
 
     this.fragment.append(page);
   }
